9d70d820f15c5d3306b6e7d68460140cf99eb189
dabin <db021129@naver.com>
2023-10-11 23:22:13 +0900

    환경설정

---
+++ db_project/src/test/java/db_project/db_project/DbProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package db_project.db_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
