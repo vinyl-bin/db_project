c22cb8e4008efeb2a2e4ec0aa01182b1201f0290
dabin <db021129@naver.com>
2023-10-14 18:50:42 +0900

    FeedService 완성

---
+++ db_project/src/main/java/db_project/db_project/service/FeedService.java
@@ -0,0 +1,27 @@
+package db_project.db_project.service;
+
+import db_project.db_project.domain.Feed;
+import db_project.db_project.repository.FeedRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class FeedService {
+
+    private final FeedRepository feedRepository;
+
+    @Transactional
+    public void saveFeed(Feed feed) {
+        feedRepository.save(feed);
+    }
+
+    public List<Feed> findFeeds() {
+        return feedRepository.findAll();
+    }
+
+}
