2536bf32f224bbdce1145e67dbd09dde94343434
dabin <db021129@naver.com>
2023-11-10 11:04:07 +0900

    feat : null 허용

--- db_project/src/main/java/db_project/db_project/domain/Feed.java
+++ db_project/src/main/java/db_project/db_project/domain/Feed.java
@@ -2,8 +2,10 @@ package db_project.db_project.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.lang.Nullable;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,30 +20,52 @@ public class Feed {
     @Column(name = "feed_id")
     private Long feed_id;
 
+    @Column(nullable = true)
     private int mtralPc;
+    @Column(nullable = true)
     private float clciQy;
+    @Column(nullable = true)
     private float naQy;
+    @Column(nullable = true)
     private float dryMatter;
+    @Column(nullable = true)
     private float ashsQy;
+    @Column(nullable = true)
     private int feedSeqNo;
+    @Column(nullable = true)
     private float crfbQy;
+    @Column(nullable = true)
     private float totEdblfibrQy;
+    @Column(nullable = true)
     private int feedClCode;
+    @Column(nullable = true)
     private float ptssQy;
+    @Column(nullable = true)
     private float mitrQy;
+    @Column(nullable = true)
     private float slwtEdblfibrQy;
+    @Column(nullable = true)
     private float liacQy;
+    @Column(nullable = true)
     private int upperFeedClCode;
+    @Column(nullable = true)
     private float fatQy;
+    @Column(nullable = true)
     private float lnacQy;
+    @Column(nullable = true)
     private float vtmaQy;
+    @Column(nullable = true)
     private float trypQy;
     private String feedNm;
+    @Column(nullable = true)
     private float crbQy;
     private String feedClCodeNm;
+    @Column(nullable = true)
     private float phphQy;
+    @Column(nullable = true)
     private float protQy;
     private String originNm;
+    @Column(nullable = true)
     private float inslbltyEdblfibrQy;
 
     @OneToMany(mappedBy = "feed")
